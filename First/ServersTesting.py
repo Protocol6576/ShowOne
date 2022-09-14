a260545274f422671bbefa2f46b038fccd3bc971
Protocol6576 <eluminaty26@mail.ru>
2022-09-14 20:05:31 +0300

    Usability_upgrade_1

--- First/ServersTesting.py
+++ First/ServersTesting.py
@@ -3,20 +3,20 @@ import multiprocessing
 import time
 import sqlite3
 
-def getInfo(host, duration): # Функция для пуолчения результата от запроса к серверу. Первое значение - начало отсчета. Второе значение - длительность отсчпета
+def getServerInfo(hostId, duration): # Функция для пуолчения результата от запроса к серверу. Первое значение - начало отсчета. Второе значение - длительность отсчпета
     active = "Активен"
-    conn = sqlite3.connect(f'nordvpnTests_join{host}.db') # Создание временной БД для возможности реализации многопоточности
+    conn = sqlite3.connect(f'nordvpnTests_join{hostId}.db') # Создание временной БД для возможности реализации многопоточности
     req = conn.cursor()
 
-    req.execute(f"""CREATE TABLE IF NOT EXISTS caServers{host}( 
+    req.execute(f"""CREATE TABLE IF NOT EXISTS caServers{hostId}( 
     serverId INT PRIMARY KEY, 
     serverIp TEXT, 
     serverName TEXT, 
     serverStatus TEXT); 
     """)
 
-    for i in range (duration): # Прогон от host до duration всех серверов
-        siteName = 'ca' + str(i + host + 1) + '.nordvpn.com' # "Впихивание" сайта в отдельную переменную
+    for i in range (duration): # Прогон от hostId до duration всех серверов
+        siteName = 'ca' + str(i + hostId + 1) + '.nordvpn.com' # "Впихивание" сайта в отдельную переменную
         try:
             ips = socket.gethostbyname(siteName)
         except socket.gaierror:
@@ -26,7 +26,7 @@ def getInfo(host, duration): # Функция для пуолчения резу
         else:
             active = "Не активен"
         
-        req.execute(f"""INSERT INTO caServers{host}(serverId, serverIp, serverName, serverStatus) VALUES({i}, '{ips}', '{siteName}', '{active}'); 
+        req.execute(f"""INSERT INTO caServers{hostId}(serverId, serverIp, serverName, serverStatus) VALUES({i}, '{ips}', '{siteName}', '{active}'); 
             """) # Ввод данных в таблицу. Счетчик - i. ips - результат поиска. active - вычисляется по наличию ips выше
     conn.commit()
 
@@ -41,33 +41,24 @@ if __name__ == '__main__':
     serverStatus TEXT); 
     """)
 
-    req = conn.cursor()
-
     timer = time.time()
 
-    duration = 2500 # Количество прогонов в одном потоке
+    req = conn.cursor()
+    servertestNumber = 10000 # Количество прогонов в одном потоке
+    multiprocNumber = 20 # Количество потоков
 
-    # Потоки
-    p1 = multiprocessing.Process(target=getInfo, args=(1, duration,))
-    p1.start()
+    mpDuration = servertestNumber // multiprocNumber # Вычисление, сколько серверов обработает один поток
+    stream = [0] * multiprocNumber
+    for i in range(multiprocNumber):
+        stream[i] = multiprocessing.Process(target=getServerInfo, args=(1 + mpDuration * i, mpDuration,))
+        stream[i].start()
 
-    p2 = multiprocessing.Process(target=getInfo, args=(2501, duration,))
-    p2.start()
+    for i in range(multiprocNumber):
+        stream[i].join()
 
-    p3 = multiprocessing.Process(target=getInfo, args=(5001, duration,))
-    p3.start()
-
-    p4 = multiprocessing.Process(target=getInfo, args=(7501, duration,))
-    p4.start()
-
-    p1.join()
-    p2.join()
-    p3.join()
-    p4.join()
-
-    for i in range (4): # Объединение временных БД в одну итоговуювуукава
-        req.execute(f"""ATTACH 'nordvpnTests_join{1 + 2500 * i}.db' as db;""")
-        req.execute(f"INSERT INTO caServers (serverId, serverIp, serverName, serverStatus) SELECT serverId + {1 + 2500 * i}, serverIp, serverName, serverStatus FROM caServers{1 + 2500 * i};")
+    for i in range (multiprocNumber): # Объединение временных БД в одну итоговуювуукава
+        req.execute(f"""ATTACH 'nordvpnTests_join{1 + mpDuration * i}.db' as db;""")
+        req.execute(f"INSERT INTO caServers (serverId, serverIp, serverName, serverStatus) SELECT serverId + {1 + mpDuration * i}, serverIp, serverName, serverStatus FROM caServers{1 + mpDuration * i};")
         conn.commit()
         req.execute(f"""DETACH db;""")
 
